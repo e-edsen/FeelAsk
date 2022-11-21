625bafa3d5cbea64f4f88fe2f5ed197ee943a74c
edsen <edy.setiawan213@gmail.com>
2022-11-21 08:56:57 +0700

    final 1.1

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -27,7 +27,7 @@
                     {{-- <input type="text" name="search" placeholder="Search here..." class="input input-bordered input-info mt-3 px-40 items-start" /> --}}
                     <div class="form-control">
                         <div class="input-group mt-3">
-                            <input type="text" placeholder="Search…" class="input input-bordered w-96" />
+                            <input type="text" name="search" placeholder="Search…" class="input input-bordered w-96" />
                             <button class="btn btn-square bg-blue-500 outline-none border-none">
                             <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
                             </button>
